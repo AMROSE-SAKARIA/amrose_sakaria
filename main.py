627e3e912acecc341b15ea8e4c34222844c7ade1
Abm32 <110992125+Abm32@users.noreply.github.com>
2022-11-05 22:36:51 +0530

    Bot
    Changed

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN") # do configure your TOKEN in a .env file
+client = commands.Bot(command_prefix="#", intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"{client.user} is logged in")
+
+@client.command()
+async def hi(ctx):
+    await ctx.reply("hello")
+
+@client.command()
+async def how(ctx):
+    await ctx.reply("I'm fine😄")
+
+client.run(TOKEN)
